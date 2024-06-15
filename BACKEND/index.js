0f839c076732ddec601996a3f97c78560e550ad9
harshit <chicholikarharshit@gmail.com>
2024-06-15 10:21:43 +0530

    almost done index.js (BE)

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,11 +1,12 @@
 const express =  require("express");
 const { createtodoschema,updatetodoschema } = require("./types");
+const { todo } = require("./db");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post("/todos",function(req,res){
+app.post("/todos",async function(req,res){
   const createpayload = req.body;
   const parsedpayload = createpayload.safeParse(createtodoschema);
   if(!parsedpayload.success){
@@ -14,15 +15,26 @@ app.post("/todos",function(req,res){
     });
     return;
   }
+  await todo.create({
+    title : createpayload.title,
+    description : createpayload.description,
+    completed : false
+  });
+  res.json({
+    msg : "instance added successfully"
+  });
 
 
 })
 
-app.get("/todos",function(req,res){
-  
+app.get("/todos",async function(req,res){
+  const todos = await todo.find({});
+  res.json({
+    todos : todos
+  });
 })
 
-app.put("/completed",function(req,res){
+app.put("/completed",async function(req,res){
   const updatepayload = req.body;
   const parsedpayload = updatepayload.safeParse(updatetodoschema);
   if(!parsedpayload.success){
@@ -30,4 +42,14 @@ app.put("/completed",function(req,res){
       msg : "bad input"
     });
     return;
+  }
+  await todo.updateOne({
+    _id : req.body.id
+  },
+  {
+    completed : true
+  });
+  res.json({
+    msg : "todo marked as completed"
+  })
 })
